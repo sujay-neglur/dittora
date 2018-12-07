4869610eb109bda1d9d2bf6db53a937aa54876cc
sujayneglur123 <sujayneglur123@gmail.com>
2018-12-07 05:20:01 -0500

    heroku mongo

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //mongodb://admin:password123@ds149252.mlab.com:49252/heroku_s6n7rkj9
 //mongodb://localhost:27017/GooRoo'
-mongoose.connect('mongodb://localhost:27017/GooRoo',{
+mongoose.connect('mongodb://heroku_t58pbvnk:gpnvsmip6mhtu2mp7ktp5ad9dg@ds133152.mlab.com:33152/heroku_t58pbvnk',{
     useNewUrlParser:true,
     useCreateIndex:true
 });
